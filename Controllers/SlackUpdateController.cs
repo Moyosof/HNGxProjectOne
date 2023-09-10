7cfe5631e582406b974784615d59e24046e54ee4
moyosof <iamthaizzie@gmail.com>
2023-09-10 13:59:14 +0100

    first task

---
+++ Controllers/SlackUpdateController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+using System.Xml;
+
+namespace HNGx.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SlackUpdateController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetInformation([FromQuery] string slackName, string track)
+        {
+            try
+            {
+                var currentDayOfWeek = DateTime.UtcNow.DayOfWeek.ToString();
+
+                var utcNow = DateTime.UtcNow;
+                var isValidUtc = Math.Abs((utcNow - DateTime.UtcNow).TotalHours) <= 2;
+
+                if (!isValidUtc)
+                {
+                    return BadRequest("UTC time is not within +/-2 hours.");
+                }
+
+                var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                var codeFileUrl = "https://github.com/Tope-stack/HngXTask1/blob/master/Controllers/InfoController.cs";
+                var sourceCodeUrl = "https://github.com/Tope-stack/HngXTask1";
+
+                var response = new
+                {
+                    SlackName = slackName,
+                    DayOfWeek = currentDayOfWeek,
+                    UTCTime = utcNow,
+                    Track = track,
+                    CodeFileUrl = codeFileUrl,
+                    SourceCodeUrl = sourceCodeUrl
+                };
+
+                return Ok(JsonConvert.SerializeObject(response, Newtonsoft.Json.Formatting.Indented));
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+
+    
+}
---
+++ SlackUpdate.cs
@@ -0,0 +1,13 @@
+﻿namespace HNGx
+{
+    public class SlackUpdate
+    {
+        public string? slack_name { get; set; }
+        public DateOnly Date { get; set; }
+        public string current_day { get; set; }
+        public string track { get; set; }
+        public string github_file_url { get; set; }
+        public string github_repo_url { get; set; }
+
+    }
+}
